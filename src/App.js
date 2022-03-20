fe9dcb6af7d4fbab4e8937bd9750ab9f2990cb88
Dev Gupta <devgupta1907@gmail.com>
2022-03-20 21:01:57 +0530

    injected navigation base structure with routes and outlet feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import Home from './routes/home/home.component';
 import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
+
+const Shop = () => {
+  return (
+    <div>
+      <div>
+        <h1> You're at Shop Page. Happy Shopping!!</h1>
+      </div>
+    </div>
+  )
+}
 
 const App = () => {
   
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
     
   );
